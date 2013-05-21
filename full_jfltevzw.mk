306674a3915c2973cebe1d370ab828d1162b9aec
XDA Garwynn <xdagarwynn@gmail.com>
2013-05-21 14:36:04 -0400

    Update full_jfltevzw.mk

--- full_jfltevzw.mk
+++ full_jfltevzw.mk
@@ -24,11 +24,11 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from jfltevzw device
-$(call inherit-product, device/samsung/jfltespr/device.mk)
+$(call inherit-product, device/samsung/jfltevzw/device.mk)
 
 # VZW APN List
 PRODUCT_COPY_FILES += \
-    device/samsung/d2vzw/prebuilt/common/etc/apns-conf-vzw.xml:system/etc/apns-conf.xml
+    device/samsung/jfltevzw/prebuilt/common/etc/apns-conf-vzw.xml:system/etc/apns-conf.xml
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := cm_jfltevzw
